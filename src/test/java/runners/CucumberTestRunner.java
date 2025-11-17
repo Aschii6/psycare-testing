c31abf56496f01cadf19e90e8cc7e4fff24c6dbb
Aschii6 <danutlechea2003@yahoo.com>
2025-11-17 23:23:26 +0200

    Create test report

--- src/test/java/runners/CucumberTestRunner.java
+++ src/test/java/runners/CucumberTestRunner.java
@@ -6,5 +6,6 @@ import org.junit.platform.suite.api.*;
 @IncludeEngines("cucumber")
 @SelectPackages("features")
 @ConfigurationParameter(key = "cucumber.glue", value = "steps, hooks")
+@ConfigurationParameter(key = "cucumber.plugin", value = "pretty, html:target/cucumber-reports.html")
 public class CucumberTestRunner {
 }
