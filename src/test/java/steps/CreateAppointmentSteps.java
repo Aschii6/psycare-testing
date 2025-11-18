0cee321f074e8df22c54090c6fee859e37e95299
Aschii6 <danutlechea2003@yahoo.com>
2025-11-18 16:48:07 +0200

    Remove Optional

--- src/test/java/steps/CreateAppointmentSteps.java
+++ src/test/java/steps/CreateAppointmentSteps.java
@@ -7,40 +7,33 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.util.Optional;
 
 public class CreateAppointmentSteps {
-    Optional<Boolean> detailsCompleted;
-    Optional<Boolean> appointmentCreated;
+    boolean detailsCompleted;
+    boolean appointmentCreated;
 
     @Given("the psychologist completed the appointment data and invited participants")
     public void psychologist_completed_appointment_data_and_invited_participants() {
         IO.println("Psychologist has completed the appointment data and invited participants");
-        detailsCompleted = Optional.of(true);
+        detailsCompleted = true;
     }
 
     @Given("the psychologist has not completed the appointment data or invited participants")
     public void psychologist_not_completed_appointment_data_or_invited_participants() {
         IO.println("Psychologist has NOT completed the appointment data or invited participants");
-        detailsCompleted = Optional.of(false);
+        detailsCompleted = false;
     }
 
     @When("they try to create the appointment")
     public void psychologist_creates_appointment() {
         IO.println("Psychologist is creating the appointment");
 
-        if (detailsCompleted.isEmpty()) {
-            throw new IllegalStateException("Appointment details completion status is not set");
-        } else {
-            appointmentCreated = detailsCompleted;
-        }
+        appointmentCreated = detailsCompleted;
     }
 
     @Then("the appointment is created successfully")
     public void appointment_is_created() {
         IO.println("The appointment has been successfully created");
-        appointmentCreated.ifPresent(
-            created -> {
-                assertThat(created).isTrue();
-            }
-        );
+
+        assertThat(appointmentCreated).isTrue();
     }
 
     @Then("ensure a rejection message is displayed")
@@ -51,10 +44,7 @@ public class CreateAppointmentSteps {
     @But("the appointment is not created")
     public void appointment_not_created() {
         IO.println("The appointment has NOT been created");
-        appointmentCreated.ifPresent(
-            created -> {
-                assertThat(created).isFalse();
-            }
-        );
+
+        assertThat(appointmentCreated).isFalse();
     }
 }
