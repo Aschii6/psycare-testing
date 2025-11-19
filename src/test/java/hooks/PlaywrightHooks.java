e5e3c7dcaf36e3d10df3edb16afc47deff6a5df7
Aschii6 <danutlechea2003@yahoo.com>
2025-11-19 11:42:32 +0200

    Change hooks a bit

--- src/test/java/hooks/PlaywrightHooks.java
+++ src/test/java/hooks/PlaywrightHooks.java
@@ -2,7 +2,9 @@ package hooks;
 
 import com.microsoft.playwright.*;
 import io.cucumber.java.After;
+import io.cucumber.java.AfterAll;
 import io.cucumber.java.Before;
+import io.cucumber.java.BeforeAll;
 
 public class PlaywrightHooks {
     public static Playwright playwright;
@@ -10,16 +12,25 @@ public class PlaywrightHooks {
     public static BrowserContext context;
     public static Page page;
 
-    @Before
-    public void setup() {
+    @BeforeAll
+    public static void setup() {
         playwright = Playwright.create();
         browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(true));
         context = browser.newContext();
+    }
+
+    @Before
+    public void createNewPage() {
         page = context.newPage();
     }
 
     @After
-    public void teardown() {
+    public void closePage() {
+        page.close();
+    }
+
+    @AfterAll
+    public static void teardown() {
         context.close();
         browser.close();
         playwright.close();
